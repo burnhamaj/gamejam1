c8b741f91e4c879d295d189732a783b675765b1c
burnhamaj <burnhamaustin@gmail.com>
2018-02-03 11:21:44 -0700

    First Commit. Working paddle with inertia and walls.

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float moveSpeed;
+
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector2 movement = new Vector2 (moveHorizontal, 0);
+		rb2d.AddForce (movement * moveSpeed);
+	}
+
+}
