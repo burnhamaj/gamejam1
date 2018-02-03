72ac0d0adf0ce6a730e72da1d693a63297aa5c3d
burnhamaj <burnhamaustin@gmail.com>
2018-02-03 13:12:16 -0700

    The ball bounces around and hist the walls and paddle.

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -18,4 +18,23 @@ public class ball : MonoBehaviour {
 	void Update () {
 		rb2d.position += moveDirection * speed * Time.deltaTime;
 	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		System.Console.WriteLine("Collision!");
+
+		// contactPoints.point  // This it the point of impact
+		// contactPoints.normal // This is the vector perpendicular to the impact
+
+		// First, find out the difference in our positions
+		Vector2 diff = other.offset;//rb2d.position - other.offset;
+
+		if (Mathf.Abs (diff.x) > Mathf.Abs (diff.y)) {
+			// The x component is more significant
+			moveDirection.x *= -1;
+		} else {
+			moveDirection.y *= -1;
+		}
+
+	}
 }
