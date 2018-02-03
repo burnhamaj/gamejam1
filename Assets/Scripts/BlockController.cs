0e6bea757bd39c4b8ddacf11ddfbf9374afa0817
burnhamaj <burnhamaustin@gmail.com>
2018-02-03 14:24:15 -0700

    Ball bounces and destroys blocks.

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockController : MonoBehaviour {
+	
+	private Rigidbody2D rb2d;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Destroy(gameObject);
 	}
 	
 	// Update is called once per frame
